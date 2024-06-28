45cacebbd508c495435e3813006a9a0c1f432981
Yash Bhardwaj <149149660+yash9939@users.noreply.github.com>
2024-06-28 16:12:36 +0530

    27/06/24 - Pandas Practice

--- Pandas1.py
+++ Pandas1.py
@@ -43,17 +43,37 @@ print(s_from_d) #Output: Series from Dictionary :
 #                        Neymar Jr.            79
 #                        dtype: int64
 
+# Pandas Dataframe
 
+# Importing the mysql connector
 import mysql.connector
+
+# Definig and establishing a object to the preferd database
 engine = mysql.connector.connect(host="localhost", user="root", password="yash@7940", database="apna")
-cur = engine.cursor()
+
+# Checking if the database is connected or not
 if engine.is_connected():
     print("Succesful") #Output:succesful
 else:
     print("Unsucessful")
-d1 = pd.read_sql_query("SELECT * FROM user", engine)
-print(d1)
 
-# data = pd.read_sql("C:\Users\nitin\Documents\MySQL Projects\BYJUS.sql")
-# d = pd.DataFrame(data)
-# print(d)
+#initialising the cursor
+cur = engine.cursor()
+
+# Reading and decoding queries to display the table data
+d1 = pd.read_sql_query("SELECT * FROM user", engine)
+
+# Initialising the data frame
+d2 = pd.DataFrame(d1)
+print(d2)
+
+# Dataframe from Dictionary
+foot = {"Footballer_ID": ["F-101", "F-102", "F-103", "F-104", "F-105", "F-106", "F-107"],
+        "Name": ["Cristiano Ronaldo", "Sunil Chhetri", "Lionel Messi", "Neymar Jr.", "Kevin De Bryune", "Mesut Ozil", "Sergio Ramos"],
+        "Age": [39, 39, 37, 32, 32, 35, 38],
+        "Goals_Scored": [895, 273, 835, 476, 137, 114, 117],
+        "Club": ["Al-Nassr FC", "Bengaluru FC", "Inter- Miami", "Al Hilal", "Manchester City FC", "Fenerbehce FC", "Sevilla FC"],
+        "Country": ["Portugal", "India", "Argentina", "Brazil", "Belgium", "Germany", "Spain"]}
+
+d3 = pd.DataFrame(foot, index=[1, 2, 3, 4, 5, 6, 7])
+print(d3)
